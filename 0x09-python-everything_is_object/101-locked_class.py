bd0d20a70e05f05e57635a081846fd97b29ad205
bennard6 <tannabennard@gmail.com>
2024-01-05 04:43:26 -0800

    0x09-python-everything_is_object

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+"""Creates instances of the class LockedClass"""
+
+
+class LockedClass:
+    """Creates instances of the class LockedClass"""
+
+    __slots__ = ['first_name']
